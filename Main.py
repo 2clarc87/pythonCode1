847bae7c76e6c77230c23cf709161986b7eece58
CHARLES CLARK <2clarc87@solent.ac.uk>
2023-09-26 12:41:32 +0100

    Choice Menu

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
-print("What is your name:\n")
-name = input()
-print(f"Hello {name}")
-print("What is your lat name")
-lastName = input()
-print(f"Hello {name} {lastName}")
+while(True):
+    choice = int(input("What do you want to do:\n1)Name\n2)Health\n"))
+    if choice == 1:
+        name = input("What is your name:")
+        lastName = input("What is your last name:")
+        print(f"Hello {name} {lastName}")
+    elif choice == 2:
+        health = "♥" * int(input("Number of lives:"))
+        energy = "♦" * int(input("Number of lives:"))
+        shield = "♦" *int(input("Number of lives:"))
+        print(f"Health:{health}")
+        print(f"Health:{energy}")
+        print(f"Health:{shield}")
+    else:
+        break
