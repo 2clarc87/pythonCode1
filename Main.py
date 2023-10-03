37bcead430d2e54496cc6c80c34a145aa9115b1e
CHARLES CLARK <2clarc87@solent.ac.uk>
2023-10-03 12:06:27 +0100

    Drawing Game

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
+import math
 while(True):
-    choice = int(input("\n\nWhat do you want to do:\n1)Name\n2)Health\n3)Robot Game\n4)Age Check\n5)Count\n"))
+    choice = int(input("\n\nWhat do you want to do:\n1)Name\n2)Health\n3)Robot Game\n4)Age Check\n5)Count\n6)Game\n"))
     if choice == 1:
         name = input("What is your name:")
         lastName = input("What is your last name:")
@@ -17,13 +18,34 @@ while(True):
         print(f"##########\n#  {eyes}  {eyes}  #\n#  {mouth*4}  #\n##########")
     elif choice == 4:
         age = int(input("How old are you:"))
-        if (age >= 18):
+        if age >= 18:
             print("You are an Adult.")
         else:
             print("You are a child.")
     elif choice == 5:
         number = int(input("What do you want to count to:"))
-        for i in range (number):
+        for i in range(number):
             print(i+1)
+    elif choice == 6:
+        array = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]]
+        while True:
+            for j in range(3):
+                print("")
+                for i in range(3):
+                    print(array[i][2-j], end="")
+            print("\n")
+            enter = int(input("Enter what you what to change:"))
+            if enter == 0: break
+            if enter <= 3:
+                y = 0
+            elif enter <= 6:
+                y = 1
+            else:
+                y = 2
+            x = (enter % 3)-1
+            if array[x][y] == "X":
+                array[x][y] = " "
+            else:
+                array[x][y] = "X"
     else:
         break
