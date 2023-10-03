a5e787ca456e48c616a0bf7946129c227acc09e2
CHARLES CLARK <2clarc87@solent.ac.uk>
2023-10-03 11:15:46 +0100

    Count

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 while(True):
-    choice = int(input("\n\nWhat do you want to do:\n1)Name\n2)Health\n3)Robot Game\n"))
+    choice = int(input("\n\nWhat do you want to do:\n1)Name\n2)Health\n3)Robot Game\n4)Age Check\n5)Count\n"))
     if choice == 1:
         name = input("What is your name:")
         lastName = input("What is your last name:")
         print(f"Hello {name} {lastName}")
     elif choice == 2:
         health = "♥" * int(input("Number of lives:"))
-        energy = "♦" * int(input("Number of lives:"))
-        shield = "♦" *int(input("Number of lives:"))
+        energy = "♦" * int(input("Amount of energy:"))
+        shield = "♦" *int(input("Amount of shield:"))
         print(f"Health:{health}")
         print(f"Health:{energy}")
         print(f"Health:{shield}")
@@ -15,6 +15,15 @@ while(True):
         eyes = input("What do you want the eyes to be:")
         mouth = input("what do you want the mouth to be:")
         print(f"##########\n#  {eyes}  {eyes}  #\n#  {mouth*4}  #\n##########")
-
+    elif choice == 4:
+        age = int(input("How old are you:"))
+        if (age >= 18):
+            print("You are an Adult.")
+        else:
+            print("You are a child.")
+    elif choice == 5:
+        number = int(input("What do you want to count to:"))
+        for i in range (number):
+            print(i+1)
     else:
         break
